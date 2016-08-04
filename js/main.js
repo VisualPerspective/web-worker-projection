8ad2fee129d6149bd816169ec219909d7ff98b0d
Kevin James <kevinwjames@gmail.com>
2016-08-04 14:56:51 -0400

    start refactoring code

--- js/main.js
+++ js/main.js
@@ -1,114 +1,95 @@
-function benchmark (
-  useWorker,
-  detail
-) {
-  document.querySelector('fieldset').disabled = true
-  var worker = new Worker('js/worker.js')
+function benchmark (useWorker, detail) {
+  d3.json("data/vectors-" + detail + ".json", function (error, vectors) {
+    runBenchmark(useWorker, detail, vectors)
+  })
+}
 
-  d3.selectAll('svg').remove()
-  var svg = d3.select('.right').append('svg')
-  var width = svg.node().getBoundingClientRect().width
-  var height = width
-
-  svg.style('height', height + 'px')
-    .attr('width', width)
-    .attr('height', height)
-
+function runBenchmark (useWorker, detail, vectors) {
+  var svg = initSVG()
+  var width = svg.attr('width')
+  var height = svg.attr('height')
   var latitude = 0, longitude = 0
-  var workerProjecting = false
   var distance = 2.0
   var projection = satellite(distance, width, height)
   var path = d3.geoPath().projection(projection)
+  var featureNames = ['countries', 'rivers', 'lakes']
+  var features = {}, paths = {}, projectedPaths = []
+  var startTime, lastTime, mainThreadTime = 0, frames = 0
+  var workerProjecting = false, worker
 
-  svg.append("path").datum({ type: "Sphere" })
-    .attr("class", "globe").attr("d", path);
+  featureNames.forEach(function (name) {
+    features[name] = topojson.feature(vectors, vectors.objects[name])
+    paths[name] = svg.append("path")
+      .datum(features[name])
+      .attr("d", path)
+      .attr("class", name)
+  })
 
-  d3.json("data/vectors-" + detail + ".json", function(error, vectors) {
-    var countries = topojson.feature(vectors, vectors.objects.countries)
-    var rivers = topojson.feature(vectors, vectors.objects.rivers)
-    var lakes = topojson.feature(vectors, vectors.objects.lakes)
+  if (useWorker) {
+    worker = createWorker(features, distance, width, height)
+  }
 
-    var projectedPaths = [[], []]
+  render()
 
-    worker.postMessage(['setup', {
-      vectors: [countries, rivers, lakes],
-      distance: distance,
-      width: width,
-      height: height
-    }])
-
-    var countryPath = svg.append("path")
-        .datum(countries)
-        .attr("d", path)
-        .attr("class", "country");
-
-    var riverPath = svg.append("path")
-        .datum(rivers)
-        .attr("d", path)
-        .attr("class", "river");
-
-    var lakePath = svg.append("path")
-        .datum(lakes)
-        .attr("d", path)
-        .attr("class", "lake");
-
-    var startTime, lastTime, mainThreadTime = 0, frames = 0
-
-    function render () {
-      var time = window.performance.now()
-      if (!startTime) {
-        startTime = time
-        lastTime = time
-      }
-      else {
-        var frameTime = time - lastTime
-        lastTime = time
-        longitude -= frameTime / 20
-      }
-
-      if ((time - startTime) > 5000) {
-        reportResults({
-          'totalTime': time - startTime,
-          'frames': frames,
-          'mainThreadTime': mainThreadTime,
-          'useWorker': useWorker,
-          'detail': detail
-        })
-      }
-      else {
-        if (useWorker) {
-          if (!workerProjecting) {
-            frames++;
-            workerProjecting = true
-
-            worker.postMessage(['projectPaths', {
-              'rotate': [longitude, latitude, 0]
-            }])
-
-            countryPath.attr('d', projectedPaths[0])
-            riverPath.attr('d', projectedPaths[1])
-            lakePath.attr('d', projectedPaths[2])
-            window.requestAnimationFrame(render)
-          }
-          else {
-            setTimeout(render, 1)
-          }
-        }
-        else {
-          frames++
-          projection.rotate([longitude, latitude, 0])
-          riverPath.attr('d', path)
-          countryPath.attr('d', path)
-          lakePath.attr('d', path)
-          window.requestAnimationFrame(render)
-        }
-      }
-
-      if (startTime !== lastTime) {
-        mainThreadTime += (window.performance.now() - time)
-      }
+  function render () {
+    var time = window.performance.now()
+    if (!startTime) {
+      startTime = lastTime = time
+    }
+    else {
+      longitude += (time - lastTime) / 20
+      lastTime = time
     }
 
+    if ((time - startTime) > 5000) {
+      reportResults({
+        'totalTime': time - startTime,
+        'frames': frames,
+        'mainThreadTime': mainThreadTime,
+        'useWorker': useWorker,
+        'detail': detail
+      })
+    }
+    else {
+      useWorker ? workerRender() : workerlessRender()
+    }
+
+    if (startTime !== lastTime) {
+      mainThreadTime += (window.performance.now() - time)
+    }
+  }
+
+  function workerlessRender() {
+    frames++
+    projection.rotate([longitude, latitude, 0])
+    featureNames.forEach(function (name) {
+      paths[name].attr('d', path)
+    })
+
+    window.requestAnimationFrame(render)
+  }
+
+  function workerRender() {
+    if (!workerProjecting) {
+      frames++
+      workerProjecting = true
+
+      worker.postMessage(['projectPaths', {
+        'rotate': [longitude, latitude, 0]
+      }])
+
+      featureNames.forEach(function (name, i) {
+        paths[name].attr('d', projectedPaths[i])
+      })
+
+      window.requestAnimationFrame(render)
+    }
+    else {
+      setTimeout(render, 1)
+    }
+  }
+
+  function createWorker(features, distance, width, height) {
     var fns = {
       'pathsProjected': function (options) {
         workerProjecting = false
@@ -116,25 +97,37 @@ function benchmark (
       }
     }
 
+    var worker = new Worker('js/worker.js')
+
     worker.onmessage = function (e) {
       var fnName = e.data[0]
       var options = e.data[1]
       fns[fnName](options)
     }
 
-    render()
-  })
+    worker.postMessage(['setup', {
+      vectors: [features['countries'], features['rivers'], features['lakes']],
+      distance: distance,
+      width: width,
+      height: height
+    }])
 
-  function reportResults(results) {
-    var fps = results.frames / (results.totalTime / 1000)
-    var mainThreadUtilization = results.mainThreadTime / results.totalTime * 100
-    document.getElementById('results').innerHTML += '<tr>' +
-      '<td>' + results.detail + '</td>' +
-      '<td>' + results.useWorker + '</td>' +
-      '<td class="number">' + fps.toPrecision(3) + '</td>' +
-      '<td class="number">' + mainThreadUtilization.toPrecision(2) + '%</td>' +
-    '</tr>'
+    return worker
+  }
 
-    document.querySelector('fieldset').disabled = false
+  function initSVG () {
+    d3.selectAll('svg').remove()
+
+    var svg = d3.select('.right').append('svg')
+    var size = svg.node().getBoundingClientRect().width
+
+    svg.style('height', height + 'px')
+      .attr('width', size)
+      .attr('height', size)
+
+    svg.append("path").datum({ type: "Sphere" })
+      .attr("class", "globe").attr("d", path)
+
+    return svg
   }
 }

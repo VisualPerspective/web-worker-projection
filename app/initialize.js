212f5b895de3ea76c4c80c18ffb7c464c4596c4d
Kevin James <kevinwjames@gmail.com>
2016-08-05 18:15:11 -0400

    begin adding canvas support

--- app/initialize.js
+++ app/initialize.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', function () {
     e.preventDefault()
     benchmark(
       this.elements['useWorker'].checked,
+      document.querySelector('input[name="method"]:checked').value == 'svg',
       document.querySelector('input[name="detail"]:checked').value
     )
 
@@ -22,17 +23,18 @@ function reportResults(results) {
   var mainThreadUtilization = results.mainThreadTime / results.totalTime * 100
   document.getElementById('results').innerHTML += '<tr>' +
     '<td>' + results.detail + '</td>' +
+    '<td>' + results.method + '</td>' +
     '<td>' + results.useWorker + '</td>' +
-    '<td class="number">' + fps.toPrecision(3) + '</td>' +
-    '<td class="number">' + mainThreadUtilization.toPrecision(2) + '%</td>' +
+    '<td class="number">' + fps.toFixed(1) + '</td>' +
+    '<td class="number">' + mainThreadUtilization.toFixed(1) + '%</td>' +
   '</tr>'
 
   document.querySelector('fieldset').disabled = false
 }
 
-function benchmark (useWorker, detail) {
+function benchmark (useWorker, useSVG, detail) {
   json("data/vectors-" + detail + ".json", function (error, vectors) {
-     new Benchmark(useWorker, detail, vectors, reportResults)
+     new Benchmark(useWorker, useSVG, detail, vectors, reportResults)
   })
 }
 
